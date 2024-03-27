6a9f7ea8438ade32d2a3983e9cc01b31b92ca42b
Taiman Saidaliyev <taiman.saidaliev@atikz.com>
2024-03-27 09:05:33 +0500

    upd

--- src/configs/config.tsx
+++ src/configs/config.tsx
@@ -1,3 +1,3 @@
 // export const BACK_HOST = 'http://127.0.0.1:8000'
-export const BACK_HOST = 'https://taiman.pythonanywhere.com'
+export const BACK_HOST = 'https://185.129.51.83:8000'
 export const WEBSITE_NAME = 'Ez QR - '
