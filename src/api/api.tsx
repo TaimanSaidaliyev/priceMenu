dd9383fcbe54bddf5598a1b890c51ecba48fa0b1
Taiman Saidaliyev <taiman.saidaliev@atikz.com>
2024-03-27 22:12:50 +0500

    updates

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -2,23 +2,27 @@ import axios from 'axios';
 import { BACK_HOST } from '../configs/config';
 import { getCookie } from '../utils/cookie';
 
+
 const token = getCookie('token');
+const instance = axios.create({
+  httpsAgent: { rejectUnauthorized: false },
+});
 
 export class Establishment {
     static async getEstablishmentInformationById(establishment_id: string) {
-        const response = await axios.get(BACK_HOST + `/establishment/${establishment_id}/information/`);
+        const response = await instance.get(BACK_HOST + `/establishment/${establishment_id}/information/`);
         return response;
     }
     static async getEstablishmentProductList(establishment_id: string) {
-        const response = await axios.get(BACK_HOST + `/establishment/${establishment_id}/products/`);
+        const response = await instance.get(BACK_HOST + `/establishment/${establishment_id}/products/`);
         return response;
     }
     static async getEstablishmentPromotions(establishment_id: string) {
-        const response = await axios.get(BACK_HOST + `/establishment/${establishment_id}/promotions/`);
+        const response = await instance.get(BACK_HOST + `/establishment/${establishment_id}/promotions/`);
         return response;
     }
     static async updateEstablishmentInfo(establishment_id: string, body: any) {
-        const response = await axios.put(BACK_HOST + `/establishment/${establishment_id}/information/`, body, {
+        const response = await instance.put(BACK_HOST + `/establishment/${establishment_id}/information/`, body, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -30,23 +34,23 @@ export class Establishment {
 
 export class Dictionary {
     static async getMenuListByEstablishmentId(establishment_id: string) {
-        const response = await axios.get(BACK_HOST + `/dict/${establishment_id}/menu/`);
+        const response = await instance.get(BACK_HOST + `/dict/${establishment_id}/menu/`);
         return response;
     }
     static async getCategoriesByMenuId(menu_id: string) {
-        const response = await axios.get(BACK_HOST + `/dict/${menu_id}/category/`);
+        const response = await instance.get(BACK_HOST + `/dict/${menu_id}/category/`);
         return response;
     }
     static async getProductsByCategoryId(category_id: string) {
-        const response = await axios.get(BACK_HOST + `/dict/${category_id}/products/`);
+        const response = await instance.get(BACK_HOST + `/dict/${category_id}/products/`);
         return response;
     }
     static async getProductById(product_id: string) {
-        const response = await axios.get(BACK_HOST + `/dict/${product_id}/product/information/`);
+        const response = await instance.get(BACK_HOST + `/dict/${product_id}/product/information/`);
         return response;
     }
     static async getProductsByIds(body: any) {
-        const response = await axios.post(BACK_HOST + `/product/get_list/`, body);
+        const response = await instance.post(BACK_HOST + `/product/get_list/`, body);
         return response;
     }
 }
@@ -54,7 +58,7 @@ export class Dictionary {
 export class SetDict {
     //Menu
     static async addMenu(body: any) {
-        const response = await axios.post(BACK_HOST + `/add_edit/menu/establishment/`, body, {
+        const response = await instance.post(BACK_HOST + `/add_edit/menu/establishment/`, body, {
             headers: {
               'Content-Type': 'multipart/form-data',
               'Authorization': `Token ${token}`
@@ -65,7 +69,7 @@ export class SetDict {
     }
 
     static async editMenu(body: any, id: string) {
-        const response = await axios.put(BACK_HOST + `/add_edit/menu/establishment/${id}/`, body, {
+        const response = await instance.put(BACK_HOST + `/add_edit/menu/establishment/${id}/`, body, {
             headers: {
               'Content-Type': 'multipart/form-data',
               'Authorization': `Token ${token}`
@@ -76,7 +80,7 @@ export class SetDict {
     }
 
     static async getMenu(id: string) {
-        const response = await axios.get(BACK_HOST + `/add_edit/menu/establishment/${id}/`, {
+        const response = await instance.get(BACK_HOST + `/add_edit/menu/establishment/${id}/`, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -86,7 +90,7 @@ export class SetDict {
     }
 
     static async deleteMenu(id: string) {
-        const response = await axios.delete(BACK_HOST + `/add_edit/menu/establishment/${id}/`,  {
+        const response = await instance.delete(BACK_HOST + `/add_edit/menu/establishment/${id}/`,  {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -98,7 +102,7 @@ export class SetDict {
 
     //Category
     static async addCategory(body: any) {
-        const response = await axios.post(BACK_HOST + `/add_edit/category/`, body, {
+        const response = await instance.post(BACK_HOST + `/add_edit/category/`, body, {
             headers: {
               'Content-Type': 'multipart/form-data',
               'Authorization': `Token ${token}`
@@ -109,7 +113,7 @@ export class SetDict {
     }
 
     static async editCategory(body: any, id: string) {
-        const response = await axios.put(BACK_HOST + `/add_edit/category/${id}/`, body, {
+        const response = await instance.put(BACK_HOST + `/add_edit/category/${id}/`, body, {
             headers: {
               'Content-Type': 'multipart/form-data',
               'Authorization': `Token ${token}`
@@ -120,7 +124,7 @@ export class SetDict {
     }
 
     static async getCategory(id: string) {
-        const response = await axios.get(BACK_HOST + `/add_edit/category/${id}/`, {
+        const response = await instance.get(BACK_HOST + `/add_edit/category/${id}/`, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -130,7 +134,7 @@ export class SetDict {
     }
 
     static async deleteCategory(id: string) {
-        const response = await axios.delete(BACK_HOST + `/add_edit/category/${id}/`, {
+        const response = await instance.delete(BACK_HOST + `/add_edit/category/${id}/`, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -140,7 +144,7 @@ export class SetDict {
     }
 
     static async sortingCategories(body: any) {
-      const response = await axios.put(BACK_HOST + `/add_edit/categories/sorting/`, body, {
+      const response = await instance.put(BACK_HOST + `/add_edit/categories/sorting/`, body, {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Token ${token}`
@@ -153,7 +157,7 @@ export class SetDict {
 
     //Product
     static async addProductByCategory(body: any) {
-        const response = await axios.post(BACK_HOST + `/add_edit/products/`, body, {
+        const response = await instance.post(BACK_HOST + `/add_edit/products/`, body, {
             headers: {
               'Content-Type': 'multipart/form-data',
               'Authorization': `Token ${token}`
@@ -164,7 +168,7 @@ export class SetDict {
     }
 
     static async editProductByCategory(body: any, id: string) {
-      const response = await axios.put(BACK_HOST + `/add_edit/products/${id}/`, body, {
+      const response = await instance.put(BACK_HOST + `/add_edit/products/${id}/`, body, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -175,7 +179,7 @@ export class SetDict {
     }
 
     static async getProductById(id: string) {
-      const response = await axios.get(BACK_HOST + `/add_edit/products/${id}/`, {
+      const response = await instance.get(BACK_HOST + `/add_edit/products/${id}/`, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -185,7 +189,7 @@ export class SetDict {
     }
 
     static async deleteProductById(id: string) {
-      const response = await axios.delete(BACK_HOST + `/add_edit/products/${id}/`, {
+      const response = await instance.delete(BACK_HOST + `/add_edit/products/${id}/`, {
           headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': `Token ${token}`
@@ -195,7 +199,7 @@ export class SetDict {
     }
 
     static async sortingProducts(body: any) {
-      const response = await axios.put(BACK_HOST + `/add_edit/products/sorting/`, body, {
+      const response = await instance.put(BACK_HOST + `/add_edit/products/sorting/`, body, {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Token ${token}`
@@ -208,7 +212,7 @@ export class SetDict {
 
 export class User {
   static async getToken(body: any) {
-    const response = await axios.post(BACK_HOST + `/auth/token/login/`, body, {
+    const response = await instance.post(BACK_HOST + `/auth/token/login/`, body, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -218,7 +222,7 @@ export class User {
   }
 
   static async checkToken() {
-    const response = await axios.post(BACK_HOST + `/auth/token/check/`, {'token': getCookie('token') || 'asd' }, {
+    const response = await instance.post(BACK_HOST + `/auth/token/check/`, {'token': getCookie('token') || 'asd' }, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -228,7 +232,7 @@ export class User {
   }
 
   static async getEstablishmentIdByToken() {
-    const response = await axios.post(BACK_HOST + `/auth/token/get_establishment/`, {'token': getCookie('token') || 'asd' }, {
+    const response = await instance.post(BACK_HOST + `/auth/token/get_establishment/`, {'token': getCookie('token') || 'asd' }, {
         headers: {
           'Content-Type': 'application/json',
         },
--- src/configs/config.tsx
+++ src/configs/config.tsx
@@ -1,3 +1,3 @@
 // export const BACK_HOST = 'http://127.0.0.1:8000'
-export const BACK_HOST = 'https://185.129.51.83:8000'
+export const BACK_HOST = 'https://ezqr.kz:8000'
 export const WEBSITE_NAME = 'Ez QR - '
